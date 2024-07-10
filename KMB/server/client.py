0d8969230bef4c14133fa427ab96524d7d182ff3
Redkin Denis <zapobedu1075@gmail.com>
2024-07-10 17:13:52 +0300

    добавлена TCP реализация

--- KMB/server/client.py
+++ KMB/server/client.py
@@ -2,19 +2,31 @@ from codecs import encode
 from codecs import decode
 from socket import *
 
+def UDP_create_socket():
+    return socket(AF_INET, SOCK_DGRAM)
+
+def UDP_exchange_message(socket: socket, ip, port, message):
+    socket.sendto(encode(message), (ip, port))
+    modifiedMessage, serverAddress = socket.recvfrom(2048)
+    return modifiedMessage
+
+def TCP_create_socket(ip, port):
+    clientSocket = socket(AF_INET, SOCK_STREAM)
+    clientSocket.connect((ip, port))
+    return clientSocket
+
+def TCP_exchange_message(socket: socket, ip, port, message):
+    clientSocket.send(encode(message))
+    modifiedMessage = clientSocket.recv(1024)
+    return modifiedMessage
+
 serverName = '127.0.0.1'
 serverPort = 12000
-clientSocket = socket(AF_INET, SOCK_DGRAM)
 
+clientSocket = TCP_create_socket(serverName, serverPort)
 message = input('Input lowercase sentence:')
-# message = b'abcd'
-
-clientSocket.sendto(encode(message),(serverName,serverPort))
-modifiedMessage, serverAddress = clientSocket.recvfrom(2048)
+modifiedMessage = TCP_exchange_message(clientSocket, serverName, serverPort, message)
 
 print(decode(modifiedMessage))
 
 clientSocket.close()
-
-
-
--- KMB/server/server.py
+++ KMB/server/server.py
@@ -1,12 +1,35 @@
 from socket import *
 
+def UDP_create_server_socket(port):
+    serverSocket = socket(AF_INET, SOCK_DGRAM)
+    serverSocket.bind(('', port))
+    return serverSocket
+
+def TCP_create_server_socket(port):
+    serverSocket = socket(AF_INET,SOCK_STREAM)
+    serverSocket.bind(('', port))
+    serverSocket.listen(1)
+    return serverSocket
+
+def TCP_exchange_message(socket: socket, port):
+    connectionSocket, addr = socket.accept()
+    sentence = connectionSocket.recv(1024)
+    capitalizedSentence = sentence.upper()
+    connectionSocket.send(capitalizedSentence)
+    connectionSocket.close()
+
+
+def UDP_exchange_message(socket: socket, port):
+    message, clientAddress = socket.recvfrom(2048)
+    modifiedMessage = message.upper()
+    serverSocket.sendto(modifiedMessage, clientAddress)
+
 serverPort = 12000
-serverSocket = socket(AF_INET, SOCK_DGRAM)
-serverSocket.bind(('', serverPort))
+
+serverSocket = TCP_create_server_socket(serverPort)
 
 print("The server is ready to receive")
 
 while 1:
-    message, clientAddress = serverSocket.recvfrom(2048)
-    modifiedMessage = message.upper()
-    serverSocket.sendto(modifiedMessage, clientAddress)
+    TCP_exchange_message(serverSocket, serverPort)
+
